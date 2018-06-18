ee6426fe50b0dfa09d275895843b0a4ea578c097
nolansingroy <nolansingroy@gmail.com>
2018-06-18 13:27:25 -0500

    docs: Fixed srcset function in dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -155,7 +155,7 @@ class DBHelper {
   /** ---- insert imageSrcsetForResturant below----*/
 
   static imageSrcSetForResturant(restaurant) {
-    return (`/img/ ${restaurant.photograph} 1x, /img/ ${restaurant.photograph} 2x, /img/ ${restaurant.photograph} 2x`);
+    return (`/img/${restaurant.photograph} 1x, /img/${restaurant.photograph} 2x, /img/${restaurant.photograph} 2x`);
   }
 
   /**
