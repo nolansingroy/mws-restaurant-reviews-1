06c70240d42c0aa7acd1cf6a38ada427ea46192b
nolansingroy <nolansingroy@gmail.com>
2018-06-23 18:36:38 -0500

    docs: Responsive Design/ servier worker

--- js/sw.js
+++ js/sw.js
@@ -1,34 +1,35 @@
-var restaurantsCache = 'restaurants-v2';
-
+var Cache_Name = 'Cache-v2';
+var urlsToCache = [
+  '/',
+  '/restaurant.html',
+  '/index.html',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/js/main.js',
+  '/js/dbhelper.js',
+  '/js/restaurant_info.js'
+];
 self.addEventListener('install',function(event) {
+//installation steps
   event.waitUntil(
-    caches.open(restaurantsCache).then(function(cache){
-      const sources = [
-        '/',
-        '/restaurant.html',
-        '/index.html',
-        '/css/styles.css',
-        '/data/resturants.json',
-        '/img/1.jpg',
-        '/img/2.jpg',
-        '/img/3.jpg',
-        '/img/4.jpg',
-        '/img/5.jpg',
-        '/img/6.jpg',
-        '/img/7.jpg',
-        '/img/8.jpg',
-        '/img/9.jpg',
-        '/img/10.jpg',
-        '/js/main.js',
-        '/js/dbhelper.js',
-        '/js/restaurant_info.js'
-      ];
-      return cache.addAll(sources);
+    caches.open(Cache_Name).then(function(cache){
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
     console.log(event.request);
     event.respondWith(
       caches.match(event.request).then(function (response){
@@ -41,15 +42,15 @@ self.addEventListener('fetch', function (event) {
     );
 });
 
-
 self.addEventListener('activate',function (event){
   event.waitUntil(
     caches.keys().then(function(cacheNames){
       return Promise.all(
-        cacheNames.filter(function (cacheNames){
-          return cacheNames.startsWith('restaurants-v2') && cacheNames != restaurantsCache;
-        }).map(function (cacheNames){
-          return caches.delete(catchName);
+        cacheNames.filter(function (cacheName){
+          return cacheNames.startsWith('Cache-v2') && cacheName != Cache_Name;
+        }).map(function (cacheName){
+          console.log('deleting cache');
+          return caches.delete(cacheName);
         })
       )
     })
