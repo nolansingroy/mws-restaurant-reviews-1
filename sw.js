13d9169c17eee1179ff6ee39417bf13f1fde3770
Nolan Singroy <nolansingroy@gmail.com>
2018-08-14 04:06:48 -0500

    docs: updated ./ for sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
 var Cache_Name = 'Cache-v2';
 var urlsToCache = [
-  '/',
-  '/restaurant.html',
-  '/index.html',
-  '/css/styles.css',
-  '/data/restaurants.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
-  '/js/main.js',
-  '/js/dbhelper.js',
-  '/js/restaurant_info.js'
+  './',
+  './restaurant.html',
+  './index.html',
+  './css/styles.css',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+  './js/main.js',
+  './js/dbhelper.js',
+  './js/restaurant_info.js'
 ];
 
 self.addEventListener('install',function (event) {
