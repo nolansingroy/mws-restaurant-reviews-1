26c61902215b2f57c566db6fb390a5d19b941572
Nolan Singroy <nolansingroy@gmail.com>
2018-08-14 02:15:50 -0500

    docs: get db fn updated

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,9 +8,12 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
+//    const port = 8000 // Change this to your server port
 //    return `http://localhost:${port}/data/restaurants.json`;
-return `/data/restaurants.json` //hosting on github pages  
+//return `/data/restaurants.json` //hosting on github pages  
+    const filePath = '/data/restaurants.json';    
+const url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}${filePath}`;
+    return url;
 }
 
   /**
